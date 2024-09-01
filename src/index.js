5e687d789cfe5d188d72a4da9c953ba322ca87fc
Dov Azogui <dov.azogui@gmail.com>
2024-09-01 15:18:44 -0400

    tg

--- src/index.js
+++ src/index.js
@@ -54,65 +54,183 @@ const otherFoods = [
     }
 ]
 
-// Deliverable # 1 solution code
-burgers.forEach(burger => {
-    console.log(burger.name)
-})
 
-// Deliverable # 2 solution code
-burgers.find(burger => {
-    return burger.name === "Flatburger"
-})
+// burgers.forEach(burger => console.log(burger.name))
 
-// Deliverable # 3 solution code
-burgers.filter(burger => {
-    return burger.name.charAt(0) === "M"
-})
+// burgers.find(burger => burger.name === "Flatburger")
 
-// Deliverable # 4 solution code
-burgers.map(burger => {
-    return burger.description
-})
+// burgers.find(burger =>{ 
+//     return burger.name[0] === "m"
 
-// Deliverable # 5 solution code
-const restaurantMenu = document.getElementById('restaurant-menu')
+// })
 
-for(let index = 0; index < burgers.length; index++){
-    const imgElement = document.createElement('img')
-    imgElement.className = 'burger'
-    imgElement.src = burgers[index].image
-    restaurantMenu.appendChild(imgElement)
+
+
+burgers.forEach(burger => burger.description)
+
+for(burger of burgers){
+    const brg = document.createElement('img')
+    brg.className = `${burger.name}`
+    brg.src = burger.image
+    document.getElementById('restaurant-menu').appendChild(brg)
+
 }
 
-// Deliverable # 6 solution code
-otherFoods.forEach(food => {
-    const imgElement = document.createElement('img')
-    imgElement.src = food.image
-    restaurantMenu.appendChild(imgElement)
+
+otherFoods.forEach(burger => {
+    const othr = document.createElement('img')
+    othr.src = burger.image.toString()
+    restaurant-menu.appendChild(othr)
+
 })
 
-// Deliverable # 7 solution code
-const detailImageElement = document.querySelector('.detail-image')
-detailImageElement.src = burgers[0].image
 
-// Deliverable # 8 solution code
-const nameElement = document.querySelector('.name')
-nameElement.textContent = burgers[0].name
 
-// Deliverable # 9 solution code
-const descriptionDisplayElement = document.getElementById('description-display')
-descriptionDisplayElement.textContent = burgers[0].description
 
-// Deliverable # 10 solution code
-const restaurantMenuImageElements = document.querySelectorAll('div#restaurant-menu img')
 
-for(const img of restaurantMenuImageElements){
-    img.style = "border-style: solid; border-color: red; border-width: 3px"
-}
 
-// Deliverable # 11 solution code
-const burgerImages = document.getElementsByClassName('burger')
+const element = document.getElementsByClassName('name')
+element.textcontent = burgers[0].name
 
-for(const img of burgerImages){
-    img.style.borderColor = "blue"
-}
+
+
+const allimg = document.querySelectorAll()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Deliverable # 1 solution code
+// burgers.forEach(burger => {
+//     console.log(burger.name)
+// })
+
+// // Deliverable # 2 solution code
+// burgers.find(burger => {
+//     return burger.name === "Flatburger"
+// })
+
+// // Deliverable # 3 solution code
+// burgers.filter(burger => {
+//     return burger.name.charAt(0) === "M"
+// })
+
+// // Deliverable # 4 solution code
+// burgers.map(burger => {
+//     return burger.description
+// })
+
+// // Deliverable # 5 solution code
+// const restaurantMenu = document.getElementById('restaurant-menu')
+
+// for(let index = 0; index < burgers.length; index++){
+//     const imgElement = document.createElement('img')
+//     imgElement.className = 'burger'
+//     imgElement.src = burgers[index].image
+//     restaurantMenu.appendChild(imgElement)
+// }
+
+// // Deliverable # 6 solution code
+// otherFoods.forEach(food => {
+//     const imgElement = document.createElement('img')
+//     imgElement.src = food.image
+//     restaurantMenu.appendChild(imgElement)
+// })
+
+// // Deliverable # 7 solution code
+// const detailImageElement = document.querySelector('.detail-image')
+// detailImageElement.src = burgers[0].image
+
+// // Deliverable # 8 solution code
+// const nameElement = document.querySelector('.name')
+// nameElement.textContent = burgers[0].name
+
+// // Deliverable # 9 solution code
+// const descriptionDisplayElement = document.getElementById('description-display')
+// descriptionDisplayElement.textContent = burgers[0].description
+
+// // Deliverable # 10 solution code
+// const restaurantMenuImageElements = document.querySelectorAll('div#restaurant-menu img')
+
+// for(const img of restaurantMenuImageElements){
+//     img.style = "border-style: solid; border-color: red; border-width: 3px"
+// }
+
+// // Deliverable # 11 solution code
+// const burgerImages = document.getElementsByClassName('burger')
+
+// for(const img of burgerImages){
+//     img.style.borderColor = "blue"
+// }
